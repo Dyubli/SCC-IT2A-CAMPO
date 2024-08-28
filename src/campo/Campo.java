8cc4447ad8bc979811f9e912dfd8c71be5bb2f80
quenyfrecel <quenyfrecelw@gmail.com>
2024-08-28 12:06:11 +0800

    wapa human

--- src/campo/Campo.java
+++ src/campo/Campo.java
@@ -1,15 +1,35 @@
 package campo;
 
+import java.util.Scanner;
+
 public class Campo {
     
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        Product[] pr = new Product [100];
         
-        Product pr = new Product ();
-        pr.addProduct(1011, "Milk", 10, 100);
-        pr.viewProduct ();
+        int nump, i;
         
-        Product pr1 = new Product ();
-        pr1.addProduct (1012, "Soap", 10, 20);
-        pr1.viewProduct();
+        System.out.print("Enter no. of products: ");
+        nump = sc.nextInt();
+        double ttp = 0;
+       
+            
+            for ( i = 0; i < nump; i++){
+                System.out.println("Enter details of Product"+ (i+1)+":");
+                System.out.print("ID: ");
+                int id = sc.nextInt();
+                System.out.print("Name: ");
+                String name = sc.next();
+                System.out.print("Price: ");
+                double pri = sc.nextDouble();
+                System.out.print("Stock: ");
+                int st = sc.nextInt();
+                System.out.println("");
+                
+                
+            }
     }
+        
+  
 }
--- src/campo/Product.java
+++ src/campo/Product.java
@@ -3,23 +3,24 @@ package campo;
 public class Product {
     
     String pname;
-    int stocks, id;
+    int stocks, id, sold;
     double price;
     
-      public void addProduct(int id, String pname, int stocks, double price){
+      public void addProduct(int id, String pname, int stocks, double price,int psold ){
           
            this.pname = pname;
            this.price = price;
            this.stocks = stocks;
            this.id = id;
+           this.sold = psold;
     }
       
       public void viewProduct() {
-          
-          double tep = this.price * this.stocks;
+          double profit = this.price * this.sold;
+          double tep = this.stocks * this.price;
           String status = ( this.stocks > 0) ? "Avilable" : "Out-of-stock";
                  
-                System.out.printf ("%-10d %-10s %-10d %-10.2f %-10.2f %-10s\n",
-                        this.id, this.pname, this.stocks, this.price, tep, status);
-    }
+                System.out.printf ("%-10d %-10s %-10.2f %-10d %-10d %10.2f %10.2f %-10s\n",
+                        this.id, this.pname, this.price, this.stocks, this.sold, profit , tep, status);
+      }
 }
