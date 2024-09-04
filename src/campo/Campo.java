4e4d096663bd4411fe3ca059bdabfbcf74eda40f
myy542 <mylenesellar13>
2024-09-04 11:07:52 +0800

    new

--- src/campo/Campo.java
+++ src/campo/Campo.java
@@ -5,40 +5,53 @@ import java.util.Scanner;
 public class Campo {
     
     public static void main(String[] args){
-        Scanner sc = new Scanner (System.in);
-        Product[] pr = new Product [100];
+
+        Scanner scanner = new Scanner(System.in);
         
-        int nump, i;
-        
-        System.out.print("Enter no. of products: ");
-        nump = sc.nextInt();
-        double ttp = 0, tep = 0;
-       
-            for ( i = 0; i < nump; i++){
-                System.out.println("\nEnter details of Product "+ (i+1)+":");
-                System.out.print("ID: ");
-                int id = sc.nextInt();
-                System.out.print("Name: ");
-                String name = sc.next();
-                System.out.print("Stock: ");
-                int st = sc.nextInt();
-                System.out.print("Price: ");
-                double pri = sc.nextDouble();
-                System.out.print("Sold: ");
-                int sd = sc.nextInt();
-                pr[i] = new Product();
-                pr[i].addProduct(id, name, st, pri, sd); 
+        System.out.print("Enter number of accounts to register: ");
+        int numberOfAccounts = scanner.nextInt();
+        scanner.nextLine();
+
+        Accounts accounts = new Accounts(numberOfAccounts);
+
+        for (int i = 0; i < numberOfAccounts; i++) {
+            System.out.println("Enter details for account " + (i + 1) + ":");
+
+            System.out.print("First Name: ");
+            String firstName = scanner.nextLine();
+
+            System.out.print("Last Name: ");
+            String lastName = scanner.nextLine();
+
+            System.out.print("Email: ");
+            String email = scanner.nextLine();
+
+            System.out.print("Username: ");
+            String username = scanner.nextLine();
+
+            String password;
+            do {
+                System.out.print("Password: ");
+                password = scanner.nextLine();
+                if (!accounts.isPasswordValid(password)) {
+                    System.out.println("Invalid password. Please try again.");
+                }
+            } while (!accounts.isPasswordValid(password));
+
+            Account newAccount = new Account(firstName, lastName, email, username, password);
+            if (accounts.addAccount(newAccount)) {
+                System.out.println("Account added successfully.");
+            } else {
+                System.out.println("Failed to add account due to duplicate information.");
             }
-            
-            for (i = 0; i <nump; i++){
-                pr[i].viewProduct();
-                ttp = ttp + (pr[i].price * pr[i].sold);
-                tep = tep + (pr[i].price * pr[i].stocks);
-            
-            }
-            
-            System.out.println("\nTotal Profit : " +ttp);
-            System.out.println("Total TEP: " +tep);
+        }
+
+        System.out.println("\nAll registered accounts:");
+        accounts.viewAccounts();
+
+        scanner.close();
+    
     }
 }
+      
  
